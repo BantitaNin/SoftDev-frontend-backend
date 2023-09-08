a5b856087e6c8959e3c533d06422662b77a14c10
BantitaNin <ninwongwannin@gmail.com>
2023-09-09 00:59:18 +0700

    add
    first add

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LandingPage from './Pages/LandingPage';
+
+const App = () => {
+  return (
+    <div>
+      <h1>หน้าเว็ปปปปป</h1>
+      <LandingPage /> {/* Include the LandingPage component */}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Pages/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div>
+      {/* Landing Page content */}
+      <h1>this is naa web</h1>
+    </div>
+  );
+};
+
+export default LandingPage;
