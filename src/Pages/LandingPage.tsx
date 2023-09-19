bc885dba613406b2fd40f976166ddd3042b45d58
Modr3d <burit3594@gmail.com>
2023-09-19 23:45:23 +0700

    Sprint 2

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { BalanceModal } from '../Components/Common/PopupModal/BalanceModal'
+import { TopupModal } from '../Components/Common/PopupModal/TopupModal'
+import { PayoutModal } from '../Components/Common/PopupModal/PayoutModal';
 
 const LandingPage = () => {
   return (
     <div>
       {/* Landing Page content */}
       <h1>this is naa web!!!!</h1>
+      <BalanceModal iconClose="../Components/Common/PopupModal/Pics/icon_close.svg" />
+      <br></br>
+      <TopupModal
+        iconClose="../PopupModal/Pics/icon_close.png"
+        line="line-1.svg"
+        iconKeyboardArrow="icon-keyboard-arrow-down.png"
+      />
+      <br></br>
+      <PayoutModal
+        iconClose="../PopupModal/Pics/icon_close.png"
+        line="line-1.svg"
+        iconKeyboardArrow="icon-keyboard-arrow-down.png"
+      />
+
     </div>
   );
 };
